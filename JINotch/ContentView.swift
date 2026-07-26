c7fb9eb308f36365672314c7e62eb8fdbee7feab
Jintaro Nishihata <jintaro@jnishihata.com>
2026-07-26 16:05:16 +0900

    Initial Commit

---
+++ JINotch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JINotch
+//
+//  Created by Jintaro Nishihata on 2026/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JINotch/JINotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  JINotchApp.swift
+//  JINotch
+//
+//  Created by Jintaro Nishihata on 2026/07/26.
+//
+
+import SwiftUI
+
+@main
+struct JINotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
